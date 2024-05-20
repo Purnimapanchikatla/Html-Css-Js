4981cefd9b7b14a9f09f06ef29dcfa228da68fa1
Purnimapanchikatla <veerapurnimapanchikatla@gmail.com>
2024-05-20 05:20:45 -0700

    Add files via upload

---
+++ Poornima/style.js
@@ -0,0 +1,19 @@
+// function bulbon(){
+//     document.getElementById('bulb').src='assets/bulbon.png';
+// }
+// function bulboff(){
+//     document.getElementById('bulb').src='assets/bulboff.png';
+// }
+var maths=80
+if(maths>=85 && maths<=100){
+  Grade="A"
+}else if(maths>=65 && maths<=85){
+   grade="B"
+}
+else if(maths>=35 && maths<=65){
+    grade="c"
+ }
+else{
+    grade="F"
+}
+document.write(grade)
